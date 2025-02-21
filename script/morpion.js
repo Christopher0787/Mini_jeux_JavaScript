6785a91212743fc14e9edb8ca2c25a0708db2418
Christopher0787 <martinchristopher087@gmail.com>
2025-02-21 16:27:26 +0100

    JS - Création des donction init() playOnce(), initGamaBoard()

--- script/morpion.js
+++ script/morpion.js
@@ -20,8 +20,8 @@ console.log("Entrée dans morpion.js");
 }
 
 /****
- * Fonction qui initialise le plateau de jeu du morpion avec ses valeurs
- * de départ
+ * Fonction qui initialise le jeu du morpion avec pour Jouer un coup
+ * Jouer un coup
  * @param {Array} plateauDeJeu
  */
 function initGameBoard(plateauDeJeu){
@@ -46,35 +46,64 @@ function playOnce(plateauDeJeu){
     let isPlayer1 = true;
     // Déclaration des variable pour le DOM
     let dom_textArea = document.getElementById("textArea");
+    let dom_all_p = document.getElementsByTagName("p");
+    let dom_all_cells = document.getElementsByClassName("cell");
 
-    do{
+    let newP = document.createElement("p");
+    
+
+    // do{
         // quand c'est au joueur  de jouer
         if(isPlayer1 === true){
+            console.log("Joueur 1 joue");
+            
+            // Mettre à jour le texteArea
+            for(let i = dom_all_p.length - 1; i >= 0; i--){
+                    dom_all_p[i].remove();               
+            }
+            newP.innerText = "Joueur 1, A toi de jouer !";
+            dom_textArea.appendChild(newP);
+            console.log(dom_all_p);
+            dom_textArea.setAttribute("class", "playeur1");
+            
+            // Rendre le plateau cliquable
+            for (let i = 0; i < dom_all_cells.length; i++){
+                dom_all_cells[i].setAttribute("onclick", "detectCell(this)");
+                console.log(dom_all_cells[i]);
 
+               
+            }
+            console.log(dom_all_cells);
+            
+            
         }
         // Si c'est au joueur 2 de jouer
         else{
 
         }
-        isPlayer1 = !isPlayer1;
+        // isPlayer1 = !isPlayer1;
 
-    } while(hasWon === false);
-
+    // } while(hasWon === false);
 
     console.log(plateauDeJeu);
     
 }
 
-// Jouer un coup
+/****
+ * Fonction qui initialise le plateau de jeu du morpion avec ses valeurs
+ * de départ
+ * @param {Array} plateauDeJeu
+ */
+function detectCell(himSelf){
+    console.log("Houra !");
+    
+}
 
 
 // Vériffier une victoire (qui tourne un booléen)
 
-
 // Changer le texte du bouton jouer (pour mettre restart)
 
-
 // En cas de victoire se faire plaisir en CSS
-
 
 //  Alterner entre un thème bleu ou rouge selon le joueur qui joue
